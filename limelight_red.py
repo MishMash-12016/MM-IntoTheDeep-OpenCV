f0fbbc804ae4f754bb6905fa9b2b78e4ba4c79e7
GOBI8450 <148621739+GOBI8450@users.noreply.github.com>
2025-03-28 12:01:25 +0300

    Update limelight_red.py

--- limelight_red.py
+++ limelight_red.py
@@ -186,8 +186,8 @@ def runPipeline(frame, llrobot):
         hsv_denoised = cv2.GaussianBlur(hsv, (5, 5), 0)
 
         # Create mask for yellow
-        HSV_RED_LOWER = ([0, 70, 50], [10, 255, 255])
-        HSV_RED_UPPER = ([170, 70, 50], [180, 255, 255])
+        HSV_RED_LOWER = ([0, 0, 0], [2, 255, 255])
+        HSV_RED_UPPER = ([0, 0, 0], [0, 0, 0])
         red_mask_lower = cv2.inRange(hsv_denoised, np.array(HSV_RED_LOWER[0]), np.array(HSV_RED_LOWER[1]))
         red_mask_upper = cv2.inRange(hsv_denoised, np.array(HSV_RED_UPPER[0]), np.array(HSV_RED_UPPER[1]))
         red_mask = cv2.bitwise_or(red_mask_lower, red_mask_upper)
